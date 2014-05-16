5f252ee0768e966336bb41772ab02eab31322fb4
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-05-16 16:27:51 -0600

    Refactor BinaryExpression source retrieval

--- lib/codeminer/processors/binary_processor.rb
+++ lib/codeminer/processors/binary_processor.rb
@@ -1,7 +1,7 @@
 module BinaryProcessor
 
   def on_binary(left, token, right)
-    BinaryExpression.new(token, left, right, extract_src_by_token(left, right.line, right.end_column))
+    BinaryExpression.new(token, left, right, extract_src_by_tokens(left, right))
   end
 
 end
