523f3542483a6dd07f9559d05f96c2fc1e5f4dcf
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-07-09 14:47:22 -0600

    Fix CommandExpression source extraction

--- lib/codeminer/processors/command_processor.rb
+++ lib/codeminer/processors/command_processor.rb
@@ -3,11 +3,11 @@ module CodeMiner
   module CommandProcessor
 
     def on_command(token, args)
-      CommandExpression.new(token, args, extract_src_by_token(token))
+      CommandExpression.new(token, args, extract_src_by_tokens(token, args))
     end
 
     def on_command_call(receiver, delimiter, token, args)
-      CommandExpression.new(token, args, extract_src_by_token(receiver), receiver)
+      CommandExpression.new(token, args, extract_src_by_tokens(receiver, args), receiver)
     end
 
   end
